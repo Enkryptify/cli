bc5e1803cfd02c150466ae7cb78a3a3a4d334089
ali <ali@enkryptify.com>
2025-12-22 10:34:55 +0100

    fixing the script for autoCom

--- src/complete/ek.ps1
+++ src/complete/ek.ps1
@@ -1,14 +1,24 @@
 Register-ArgumentCompleter -CommandName ek -ScriptBlock {
-  param($wordToComplete, $commandAst, $cursorPosition)
+  param(
+      $wordToComplete,
+      $commandAst,
+      $cursorPosition
+  )
 
-  $args = $commandAst.CommandElements | Select-Object -Skip 1 | ForEach-Object { $_.ToString() }
+  $args = $commandAst.CommandElements |
+      Select-Object -Skip 1 |
+      ForEach-Object { $_.ToString() }
 
-  & ek __complete $args | ForEach-Object {
-      [System.Management.Automation.CompletionResult]::new(
-          $_,
-          $_,
-          'ParameterValue',
-          $_
-      )
+  $results = & ek __complete @args 2>$null
+
+  foreach ($r in $results) {
+      if ($r -like "$wordToComplete*") {
+          [System.Management.Automation.CompletionResult]::new(
+              $r,
+              $r,
+              'ParameterValue',
+              $r
+          )
+      }
   }
 }
