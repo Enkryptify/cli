005e8b38ebfe5164aa2c9c29fb58a3166f0eed65
ali <ali@enkryptify.com>
2025-12-22 10:44:11 +0100

    fixing the script for autoCom

---
+++ src/complete/EnkryptifyCompletion.psm1
@@ -0,0 +1,16 @@
+Register-ArgumentCompleter -CommandName ek -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+
+  $args = $commandAst.CommandElements |
+      Select-Object -Skip 1 |
+      ForEach-Object { $_.ToString() }
+
+  & ek __complete $args | ForEach-Object {
+      [System.Management.Automation.CompletionResult]::new(
+          $_,
+          $_,
+          'ParameterValue',
+          $_
+      )
+  }
+}
