191765e6a29a6df7d014e8257d5efdf9b988041a
ali <ali@enkryptify.com>
2025-12-22 20:30:01 +0100

    fixing github action for check and auto completion

--- src/complete/enkryptify-completion.psm1
+++ src/complete/enkryptify-completion.psm1
@@ -1,27 +1,16 @@
 Register-ArgumentCompleter -CommandName ek -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
 
-    $commandArgs = $commandAst.CommandElements |
+    $args = $commandAst.CommandElements |
         Select-Object -Skip 1 |
         ForEach-Object { $_.ToString() }
 
-    $ekPath = (Get-Command ek.exe -ErrorAction SilentlyContinue).Source
-    if (-not $ekPath) {
-        $ekPath = "ek.exe"
-    }
-
-    try {
-        & $ekPath __complete $commandArgs | ForEach-Object {
-            if ($_) {
-                [System.Management.Automation.CompletionResult]::new(
-                    $_,
-                    $_,
-                    'ParameterValue',
-                    $_
-                )
-            }
-        }
-    } catch {
-        # Return empty completions on error
+    & ek __complete $args | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new(
+            $_,
+            $_,
+            'ParameterValue',
+            $_
+        )
     }
 }
